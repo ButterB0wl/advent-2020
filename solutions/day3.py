ae3f77729519ff431d4e54a161735b62ea221c40
Artemie Jurgenson <ajurgenson@sonatype.com>
2020-12-08 16:11:05 -0500

    converted the traverser to a function to calculate number of trees for the rest of the slopes in part 2

--- solutions/day3.py
+++ solutions/day3.py
@@ -3,16 +3,29 @@ forest = list(map(lambda row: row.strip(), file.readlines()))
 right_index = len(forest[0])-1
 bottom_index = len(forest)-1
 
-x = y = 0
-tree_count = 0
+def tree_counter(x_move, y_move):
+    x = y = 0
+    tree_count = 0
+    while y <= bottom_index:
+        if x > right_index:
+            x = x - (right_index + 1)
 
-while y <= bottom_index:
-    if x > right_index:
-        x = x - (right_index + 1)
+        if forest[y][x] == '#':
+            tree_count += 1
+        x += x_move
+        y += y_move
+    return tree_count
 
-    if forest[y][x] == '#':
-        tree_count += 1
-    x += 3
-    y += 1
+print(tree_counter(1, 1))
+print(tree_counter(3, 1))
+print(tree_counter(5, 1))
+print(tree_counter(7, 1))
+print(tree_counter(1, 2))
 
-print(tree_count)
+tree_product = tree_counter(1, 1)
+tree_product *= tree_counter(3, 1)
+tree_product *= tree_counter(5, 1)
+tree_product *= tree_counter(7, 1)
+tree_product *= tree_counter(1, 2)
+
+print(tree_product)
