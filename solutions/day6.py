c01788325588215ceea3113e691fb73ca6ee1b60
Artemie Jurgenson <ajurgenson@sonatype.com>
2020-12-14 18:39:42 -0500

    added day6 part 2 modification to get form answers where everyone answered yes

--- solutions/day6.py
+++ solutions/day6.py
@@ -5,7 +5,7 @@ group_counts = list()
 
 yes_list = set()
 for form in forms:
-    if form == '\n':
+    if form == '\n' or form == forms[-1]:
         print(yes_list)
         group_counts.append(len(yes_list))
         yes_list = set()
@@ -14,8 +14,22 @@ for form in forms:
         for question in form:
             yes_list.add(question)
 
-print(yes_list)
-group_counts.append(len(yes_list))
+print(group_counts)
+print(sum(group_counts))
+
+# part 2
+
+group_counts = list()
+group_forms = list()
+for form in forms:
+    if form == '\n' or form == forms[-1]:
+        common_yes = set(group_forms[0]).intersection(*group_forms)
+        print(common_yes)
+        group_counts.append(len(common_yes))
+        group_forms = list()
+    else:
+        form = form.strip('\n')
+        group_forms.append(form)
 
 print(group_counts)
 print(sum(group_counts))
