45f8e549c7788928d31c8a99bad00b5e1a4677e2
Artemie Jurgenson <ajurgenson@sonatype.com>
2020-12-14 17:59:29 -0500

    added a couple lines to find the missing elements from the sorted seat ids compared to a full list generated from the start and end values

--- solutions/day5.py
+++ solutions/day5.py
@@ -16,24 +16,31 @@ for p in passes:
 
     row_parts = p[:7]
     col_parts = p[7:]
-    print(row_parts, col_parts)
+    # print(row_parts, col_parts)
 
     for part in row_parts:
         if part == 'F':
             rows = split_list(rows, True)
         else:
             rows = split_list(rows, False)
-        print(rows)
+        # print(rows)
 
     for part in col_parts:
         if part == 'L':
             cols = split_list(cols, True)
         else:
             cols = split_list(cols, False)
-        print(cols)
+        # print(cols)
     
     id = rows[0] * 8 + cols[0]
     seat_ids.append(id)
+    
+seat_ids = sorted(seat_ids)
+print(seat_ids)
 
-print(max(seat_ids))
+start, end = seat_ids[0], seat_ids[-1]
+missing_seat = sorted(set(range(start, end + 1)).difference(seat_ids))
+
+# print(max(seat_ids))
+print(missing_seat)
 
