93d02e4cbd7e87af7879059aeb07dea7b04c2e53
Artemie Jurgenson <ajurgenson@sonatype.com>
2020-12-15 18:08:56 -0500

    finding contiguous slice of array that adds up to a number for day 9 part 2

--- solutions/day9.py
+++ solutions/day9.py
@@ -20,4 +20,26 @@ for target in test_list:
             break
     else:
         print('no sln', target)
+        invalid_num = target
+        break
+
+# day2
+
+index = typed_list.index(invalid_num)
+target_list = typed_list[:index]
+
+left_index = 0
+right_index = 1
+
+while right_index < len(target_list):
+    cont_set = target_list[left_index:right_index+1]
+    cont_sum = sum(cont_set)
+    if cont_sum < invalid_num:
+        right_index += 1
+    elif cont_sum > invalid_num:
+        left_index += 1
+    else:
+        print('Contiguous set:', cont_set)
+        print('Contiguous sum:', cont_sum)
+        print('Encryption Weakness:', min(cont_set) + max(cont_set))
         break
